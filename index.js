17d8cb20c9ec532a70634edc6ceb63210c37ef75
Alex <alex.walker@accedo.tv>
2022-06-28 13:26:19 +1200

    Improvements

--- index.js
+++ index.js
@@ -3,105 +3,111 @@ const fs = require("fs");
 const { download } = require("./download");
 
 const mpdFileName = process.argv[2];
-const replaceMappings = [];
 const remoteMappings = [];
 
-const xml = fs.readFileSync(mpdFileName, "utf8");
-const manifest = JSON.parse(convert.xml2json(xml));
+const mpdContents = fs.readFileSync(mpdFileName, "utf8");
+const manifest = JSON.parse(convert.xml2json(mpdContents));
 
-const periodIndex = 0;
-const periods = manifest.elements[periodIndex].elements;
+const periods = manifest.elements[0].elements;
+const numPeriods = periods.length;
 
-const adaptationSets = periods[periodIndex].elements.filter(
-  (element) =>
-    element.name === "AdaptationSet" &&
-    ["video/mp4", "audio/mp4"].includes(element.attributes.mimeType)
-);
-
-const adaptationIndex = 0;
-
-const adaptationSet = adaptationSets[adaptationIndex].elements;
-const initTemplate = adaptationSet.find(
-  (element) => element.name === "SegmentTemplate"
-);
-const startNumber = Number(initTemplate.attributes.startNumber);
-const numSegments = initTemplate.elements
-  .find((element) => element.name === "SegmentTimeline")
-  .elements.filter((element) => element.attributes.r)
-  .reduce((total, element) => total + Number(element.attributes.r), 0);
-
-replaceMappings.push({
-  old: initTemplate.attributes.initialization,
-  new: `periods/${periodIndex}/$RepresentationID$/init0.m4f`,
-});
-replaceMappings.push({
-  old: initTemplate.attributes.media,
-  new: `periods/${periodIndex}/$RepresentationID$/segment$Number$.m4f`,
-});
-
-const representations = adaptationSet.filter(
-  (element) => element.name === "Representation"
-);
-for (representation of representations) {
-  // const representationIndex = 0;
-  // const representation = representations[representationIndex];
-  const initRemoteUrl = initTemplate.attributes.initialization.replace(
-    /\$RepresentationID\$/,
-    representation.attributes.id
+for (let periodIndex = 0; periodIndex < numPeriods; periodIndex++) {
+  const adaptationSets = periods[periodIndex].elements.filter(
+    (element) =>
+      element.name === "AdaptationSet" &&
+      ["video/mp4", "audio/mp4"].includes(element.attributes.mimeType)
   );
-  const initLocalPath = `periods/${periodIndex}/${representation.attributes.id}/init0.m4f`;
-  remoteMappings.push({
-    remote: initRemoteUrl,
-    path: initLocalPath,
-    period: periodIndex,
-  });
-  for (
-    let segmentNumber = startNumber;
-    segmentNumber <= startNumber + numSegments;
-    segmentNumber++
-  ) {
-    const remoteUrl = initTemplate.attributes.media
-      .replace(/\$RepresentationID\$/, representation.attributes.id)
-      .replace(/\$Number\$/, segmentNumber);
-    remoteMappings.push({
-      period: periodIndex,
-      remote: remoteUrl,
-      path: `periods/${periodIndex}/${representation.attributes.id}/segment${segmentNumber}.m4f`,
-    });
+
+  for (const eachSet of adaptationSets) {
+    const adaptationSet = eachSet.elements;
+    const initTemplate = adaptationSet.find(
+      (element) => element.name === "SegmentTemplate"
+    );
+    const startNumber = Number(initTemplate.attributes.startNumber);
+
+    const numSegments = initTemplate.elements
+      .find((element) => element.name === "SegmentTimeline")
+      .elements.reduce(
+        (total, element) =>
+          total + (element.attributes.r ? Number(element.attributes.r) : 1),
+        0
+      );
+
+    const representations = adaptationSet.filter(
+      (element) => element.name === "Representation"
+    );
+    for (representation of representations) {
+      const initRemoteUrl = initTemplate.attributes.initialization.replace(
+        /\$RepresentationID\$/,
+        representation.attributes.id
+      );
+      const initLocalPath = `periods/${periodIndex}/${representation.attributes.id}/init0.m4f`;
+      remoteMappings.push({
+        remote: initRemoteUrl,
+        path: initLocalPath,
+        period: periodIndex,
+      });
+      for (
+        let segmentNumber = startNumber;
+        segmentNumber <= startNumber + numSegments + 2;
+        segmentNumber++
+      ) {
+        const remoteUrl = initTemplate.attributes.media
+          .replace(/\$RepresentationID\$/, representation.attributes.id)
+          .replace(/\$Number\$/, segmentNumber);
+        remoteMappings.push({
+          period: periodIndex,
+          remote: remoteUrl,
+          path: `periods/${periodIndex}/${representation.attributes.id}/segment${segmentNumber}.m4f`,
+        });
+      }
+    }
+    initTemplate.attributes.initialization = `periods/${periodIndex}/$RepresentationID$/init0.m4f`;
+    initTemplate.attributes.media = `periods/${periodIndex}/$RepresentationID$/segment$Number$.m4f`;
   }
 }
 
 class DownloadQueue {
-  maxDownloads = 5;
+  maxDownloads = 10;
+  numDownloads = 0;
   items;
+  onQueueComplete;
 
-  constructor(items) {
+  constructor(items, onQueueComplete) {
     this.items = items;
-  }
-
-  init = () => {
+    this.onQueueComplete = onQueueComplete;
     for (let i = 0; i < this.maxDownloads; i++) {
       if (this.items.length) {
-        this.start(this.items.shift());
+        this.startOneDownload(this.items.shift());
       }
     }
-  };
+  }
 
-  start = (item) => {
-    console.log(`Downloading period ${item.period} ${item.path}`);
+  startOneDownload = (item) => {
+    console.log(
+      `Downloading period ${item.period + 1}/${numPeriods} ${item.path}`
+    );
+    this.numDownloads += 1;
     download(item.remote, item.path)
-      .then(this.onFinish)
+      .then(this.oneDownloadFinished)
       .catch((err) => {
-        console.warn("Error downloading", item.path, item.period);
+        console.warn("Error downloading", item.path, item.period, err);
       });
   };
 
-  onFinish = () => {
+  oneDownloadFinished = () => {
+    this.numDownloads -= 1;
     if (this.items.length) {
-      this.start(this.items.shift());
+      this.startOneDownload(this.items.shift());
+    } else if (this.numDownloads === 0) {
+      this.onQueueComplete();
     }
   };
 }
 
-const queue = new DownloadQueue(remoteMappings);
-queue.init();
+const onComplete = () => {
+  console.log("Downloads done");
+  const modifiedMpd = convert.json2xml(JSON.stringify(manifest), { spaces: 2 });
+  fs.writeFileSync("out.mpd", modifiedMpd);
+};
+new DownloadQueue(remoteMappings, onComplete);
